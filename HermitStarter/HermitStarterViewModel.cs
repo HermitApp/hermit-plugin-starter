ba87603a249f3d250109034e903599fde8d468fd
Lucas Brendel <lucasbrendel@gmail.com>
2014-12-29 16:33:51 -0600

    Fixed incorrect file name

---
+++ HermitStarter/HermitStarterViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.Composition;
+using Hermit.Plugin;
+using Caliburn.Micro;
+
+namespace HermitStarter
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Export(typeof(Shell))]
+    public class HermitStarterViewModel : Shell
+    {
+        public HermitStarterViewModel()
+        {
+            this.DisplayName = "Hermit Starter Project";
+        }
+    }
+}
