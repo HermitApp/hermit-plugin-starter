944f494c7cf9cbbbaf373e7a02a16adc478b7ea5
Lucas Brendel <lucasbrendel@gmail.com>
2014-12-28 21:01:15 -0600

    Adding base project

---
+++ HermitStarter/HermitStartViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.Composition;
+using Hermit.Plugin;
+using Caliburn.Micro;
+
+namespace HermitStarter
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Export(typeof(IPlugin))]
+    public class HermitStartViewModel : PropertyChangedBase, IPlugin
+    {
+        private string _DisplayName;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string DisplayName
+        {
+            get
+            {
+                return _DisplayName;
+            }
+            set
+            {
+                if(value != _DisplayName)
+                {
+                    _DisplayName = value;
+                    NotifyOfPropertyChange(() => DisplayName);
+                }
+            }
+        }
+
+        public HermitStartViewModel()
+        {
+            this.DisplayName = "Hermit Starter Project";
+        }
+    }
+}
